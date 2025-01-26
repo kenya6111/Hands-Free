79258b17d5125a14a9761d7d9b80e0bea210cc48
kenya6111 <kenyanke6111@gmail.com>
2025-01-26 20:43:09 +0900

    aa

---
+++ textToSpeach.js
@@ -0,0 +1,8 @@
+// export function say (text) {
+//     const play_option = new SpeechSynthesisUtterance()
+//     let speechTxt = 'テキストを読み上げています'
+//     play_option.text = speechTxt;
+//     play_option.lang = 'ja-JP';
+//     speechSynthesis.cancel();
+//     window.speechSynthesis.speak(play_option)
+// }
